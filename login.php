352789c2093342c32cba97d1f3f4c1bacd53faab
wisnumrt <wisnumurti982@gmail.com>
2024-06-06 10:52:03 +0700

    update connect to database

--- login.php
+++ login.php
@@ -16,8 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($user && password_verify($password, $user['password'])) {
         // Password is correct
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['username'] = $user['username'];
+        $_SESSION['userId'] = $user['id'];
         $_SESSION['nickname'] = $user['nickname']; 
         header('Location: dashboard.php');
         exit;
